b89e90ffea1176edebd4d8bf8dda75a988f59090
Giovane25 <99659225+Giovane25@users.noreply.github.com>
2022-05-27 14:39:27 -0300

    update curriculo jquery

---
+++ currículo/script.js
@@ -0,0 +1,4 @@
+$('.jQueryLine').html('feito por Giovane <a href="https://www.w3schools.com/w3css/default.asp" target="_blank"> Giovane </a>')
+
+var x = "Olá! Bem vindo ao currículo do Giovane"
+alert (x)
